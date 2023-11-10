0b7d05c3d76a626e52c40d4589b33f774d07ca36
lavaskiller <woohyuk@khu.ac.kr>
2023-11-11 08:07:51 +0900

    ain

--- home.py
+++ home.py
@@ -3,3 +3,9 @@ import streamlit as st
 
 def app():
     st.subheader("Main Page")
+    image = 'logo.png'
+
+    # Streamlit 컬럼을 사용하여 이미지를 중앙에 배치
+    col1, col2, col3 = st.columns([1,2,1])
+    with col2:
+        st.image(image)
