396d5584f5daa70d1b9c84833e5f02da592331f9
lavaskiller <80824061+lavaskiller@users.noreply.github.com>
2023-12-06 22:58:58 +0900

    clear

--- home.py
+++ home.py
@@ -6,6 +6,14 @@ def app():
     st.subheader("")
     image = 'logo.png'
 
+    hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
     # Streamlit 컬럼을 사용하여 이미지를 중앙에 배치
     col1, col2, col3 = st.columns([1,2,1])
     with col2:
